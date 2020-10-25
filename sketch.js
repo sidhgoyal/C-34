cb57975808e6335766d78933d7945e15db22cc68
sidhgoyal <sidhgoyal>
2020-10-25 17:55:03 +0530

    sync

--- sketch.js
+++ sketch.js
@@ -1,29 +1,49 @@
 var ball;
-
+var database;
+var pos;
 function setup(){
     createCanvas(500,500);
+     
+    database = firebase.database(); 
+
+    var posref = database.ref('ball/position');
+    posref.on("value",readpos);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+    if (pos!==undefined) {
+        if(keyDown(LEFT_ARROW)){
+        writePosition(-3,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(3,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-3);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        
+        writePosition(0,+3);
     }
+    }
+    
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    console.log(pos);
+    database.ref('ball/position').set({
+        x:pos.x+x,
+        y:pos.y+y
+    });
+    
+}
+
+function readpos(storage){
+    pos = storage.val();
+    ball.x = pos.x;
+    ball.y = pos.y;
 }
